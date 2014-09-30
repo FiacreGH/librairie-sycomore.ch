34025b057517a7f406ae12e1390a61eb8a2dcb08
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-09-30 08:09:35 +0200

    Update site configuration

---
+++ app/SculpinKernel.php
@@ -0,0 +1,13 @@
+<?php
+
+class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel
+{
+	protected function getAdditionalSculpinBundles() {
+
+		return array(
+			'Mavimo\Sculpin\Bundle\EditorBundle\SculpinEditorBundle',
+			'Mavimo\Sculpin\Bundle\RedirectBundle\SculpinRedirectBundle',
+			'Fab\Sculpin\Bundle\PagesBundle\SculpinPagesBundle',
+		);
+	}
+}
